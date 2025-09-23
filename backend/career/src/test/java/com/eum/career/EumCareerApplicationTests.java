cc2badbe1a7bda0379b6612a2d4c86fc88ad6d57
wlsksekf <jinaniyagi@gmail.com>
2025-09-23 09:01:24 +0900

    feat: Initial setup for Spring Boot and React projects

---
+++ backend/career/src/test/java/com/eum/career/EumCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eum.career;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EumCareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
