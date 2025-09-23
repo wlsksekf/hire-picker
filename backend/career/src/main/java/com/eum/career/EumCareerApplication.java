cccfb55fe91d56652bdc8507879e482dd17783ac
wlsksekf <jinaniyagi@gmail.com>
2025-09-23 09:01:24 +0900

    feat: Initial setup for Spring Boot and React projects

---
+++ backend/career/src/main/java/com/eum/career/EumCareerApplication.java
@@ -0,0 +1,13 @@
+package com.eum.career;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EumCareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EumCareerApplication.class, args);
+	}
+
+}
