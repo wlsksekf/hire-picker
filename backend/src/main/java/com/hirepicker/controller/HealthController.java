72e59ec6d8cf8a355ecf831f99b107463da0e246
park990 <rhkgkrwk2008@gmail.com>
2025-10-05 01:34:18 +0900

    Nothing Change

--- backend/src/main/java/com/hirepicker/controller/HealthController.java
+++ backend/src/main/java/com/hirepicker/controller/HealthController.java
@@ -3,8 +3,11 @@ package com.hirepicker.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 //push Test
 @RestController
+@RequiredArgsConstructor
 public class HealthController {
     @GetMapping("/health")
     public String checkHealth() {
