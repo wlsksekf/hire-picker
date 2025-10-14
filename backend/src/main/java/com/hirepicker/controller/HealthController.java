67997133bd6cb1f613bed5dd896275b39a63c49f
park990 <rhkgkrwk2008@gmail.com>
2025-10-14 09:12:09 +0900

    ㅎㅇㅎㅇ

--- backend/src/main/java/com/hirepicker/controller/HealthController.java
+++ backend/src/main/java/com/hirepicker/controller/HealthController.java
@@ -4,15 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import lombok.RequiredArgsConstructor;
-
-
-@RestController
-@RequiredArgsConstructor
-
+@Tag(name = "Health Check", description = "서버 상태 확인 API")
+@RestController @RequestMapping("/api/health")
 public class HealthController {
 
     @Operation(summary = "서버 헬스 체크", description = "서버가 정상적으로 동작하는지 확인합니다.")
