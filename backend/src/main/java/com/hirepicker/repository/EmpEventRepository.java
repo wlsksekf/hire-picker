97d0281ed90936d3270d47555a21193bed7f42b3
park990 <rhkgkrwk2008@gmail.com>
2025-10-16 16:48:03 +0900

    annotation 정의

--- backend/src/main/java/com/hirepicker/repository/EmpEventRepository.java
+++ backend/src/main/java/com/hirepicker/repository/EmpEventRepository.java
@@ -2,9 +2,11 @@ package com.hirepicker.repository;
 
 import com.hirepicker.model.EmpEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmpEventRepository extends JpaRepository<EmpEvent, Long> {
     Optional<EmpEvent> findByEventCode(String eventCode);
 }
--- backend/src/main/java/com/hirepicker/repository/JobPostingRepository.java
+++ backend/src/main/java/com/hirepicker/repository/JobPostingRepository.java
@@ -2,8 +2,11 @@ package com.hirepicker.repository;
 
 import com.hirepicker.model.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
     Optional<JobPosting> findByPostingId(String postingId);
 }
