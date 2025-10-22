de3218d468d954661102d4814ba6cd1efe546615
wlsksekf <jinaniyagi@gmail.com>
2025-10-22 18:58:14 +0900

    swagger-ui

--- backend/src/main/java/com/hirepicker/controller/AuthController.java
+++ backend/src/main/java/com/hirepicker/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.hirepicker.controller;
 import com.hirepicker.dto.LoginRequest;
 import com.hirepicker.dto.LoginResponse;
 import com.hirepicker.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation; // Added import
+import io.swagger.v3.oas.annotations.tags.Tag;   // Added import
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증", description = "사용자 인증 및 토큰 관련 API") // Added Tag
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "로그인", description = "이메일과 비밀번호를 사용하여 로그인하고 JWT 토큰을 발급받습니다.") // Added Operation
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         LoginResponse loginResponse = authService.login(loginRequest);
