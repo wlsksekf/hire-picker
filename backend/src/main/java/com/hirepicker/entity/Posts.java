f31e7dae5135ba8e9a6260f837b085a08dc7c1fd
Garam <gahramh@gmail.com>
2025-11-02 15:55:47 +0900

    11-02

--- backend/src/main/java/com/hirepicker/entity/Posts.java
+++ backend/src/main/java/com/hirepicker/entity/Posts.java
@@ -6,9 +6,7 @@ import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.time.LocalDateTime;
 
 @Entity
@@ -20,6 +18,7 @@ import java.time.LocalDateTime;
 @ToString
 @EntityListeners(AuditingEntityListener.class)
 public class Posts {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postIdx;        // BIGINT, PK, AI
@@ -28,7 +27,6 @@ public class Posts {
     private Long boardIdx;       // BIGINT, NN
 
     @Column(nullable = false)
-    @JsonProperty("pUserIdx")
     private Long pUserIdx;       // BIGINT, NN
 
     @Column(nullable = false, length = 255)
@@ -52,4 +50,15 @@ public class Posts {
 
     @Column(length = 50)
     private String imgName;      // VARCHAR(50)
+
+    // Lombok 자동 getter/setter도 남아있지만 절대로 수동으로 추가X!
+
+    // **꼭 아래처럼 getter에만 JsonProperty 걸어서 단일 키만 보장!**
+    @JsonProperty("pUserIdx")
+    public Long getPUserIdx() {
+        return pUserIdx;
+    }
+    public void setPUserIdx(Long pUserIdx) {
+        this.pUserIdx = pUserIdx;
+    }
 }
